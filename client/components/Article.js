1d4c35d01bf8c2ec63965e8cb26be8f419e6c57d
Passakun <passakun@passakuns-MacBook-Air.local>
2018-07-13 00:17:18 +0700

    Add row display flex

--- client/components/Article.js
+++ client/components/Article.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, DemoBox } from 'antd';
 import Imageblog from '../static/images/siberian.png';
 import '../styles/Article.css';
 
@@ -8,7 +8,7 @@ class Article extends Component {
     return (
       <div>
         <h1 style={{ textAlign:'center'}}> บทความ </h1>
-        <Row className='blog'>
+        <Row type="flex" justify="space-around" className='blog'>
           <Col span={12} style={{border: '1px solid'}}>
             <img
               src= {require('../static/images/siberian.png?inline')}
@@ -22,29 +22,35 @@ class Article extends Component {
             <p style={{marginBottom: 0}}>01/01/18</p>
             <p>รายละเอียดเนื้อหาาาาาากดาพนาดนพำาเนพำเานำพาเนำพเ่าำนพเ่าำรพนเ่รพำเ้ืำร้นพเืรพนำเืำรนพ</p>
           </Col>
+
           <Col span={12} style={{paddingLeft: '30px',paddingRight: '30px'}} >
-            <Col span={12} style={{border: '1px solid'}}>
-              <img
-                src= {require('../static/images/siberian.png?inline')}
-                style={{
-                  width: '100%',
-                  height: 150,
-                  marginBottom: '10px',
-                }}
-              />
-              <h3> หัวเรื่องข่าวสาร/บทความ </h3>
-            </Col>
-            <Col span={12} style={{ border: '1px solid'}}>
-              <img
-                src= {require('../static/images/siberian.png?inline')}
-                style={{
-                  width: '100%',
-                  height: 150,
-                  marginBottom: '10px',
-                }}
-              />
-              <h3> หัวเรื่องข่าวสาร/บทความ </h3>
-            </Col>
+          <Row type="flex" justify="space-between" >
+            <Col span={11} style={{border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+              <Col span={11} style={{ border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+            
+            </Row>
             
           </Col>
         </Row>
