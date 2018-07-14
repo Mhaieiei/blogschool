086c7393c77ec84c916f5957384805d95b043248
oraphan <oraphan@thinknet.co.th>
2018-07-14 15:39:21 +0700

    add article

--- client/components/Articles.js
+++ client/components/Articles.js
@@ -1,9 +1,89 @@
-import { Row, Col, Card } from 'antd'
+import React, { Component } from 'react';
+import { Row, Col, DemoBox } from 'antd';
+import Imageblog from '../static/images/siberian.png';
+import '../styles/Article.css';
 
-const { Meta } = Card
+class Article extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign:'center'}}> บทความ </h1>
+        <Row type="flex" justify="space-around" className='blog'>
+          <Col span={12} style={{border: '1px solid'}}>
+            <img
+              src= {require('../static/images/siberian.png?inline')}
+              style={{
+                width: '100%',
+                height: 300,
+                marginBottom: '10px',
+              }}
+            />
+            <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+            <p style={{marginBottom: 0}}>01/01/18</p>
+            <p>รายละเอียดเนื้อหาาาาาากดาพนาดนพำาเนพำเานำพาเนำพเ่าำนพเ่าำรพนเ่รพำเ้ืำร้นพเืรพนำเืำรนพ</p>
+          </Col>
 
-const Articles = () => (
-  <div/>
-)
+          <Col span={12} style={{paddingLeft: '30px',paddingRight: '30px'}} >
+          <Row type="flex" justify="space-between" >
+            <Col span={11} style={{border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+              <Col span={11} style={{ border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+            
+            </Row>
+          <Row type="flex" justify="space-between" >
+            <Col span={11} style={{border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+              <Col span={11} style={{ border: '1px solid'}}>
+                <img
+                  src= {require('../static/images/siberian.png?inline')}
+                  style={{
+                    width: '100%',
+                    height: 150,
+                    marginBottom: '10px',
+                  }}
+                />
+                <h3> หัวเรื่องข่าวสาร/บทความ </h3>
+                <p style={{marginBottom: 0}}>01/01/18</p>
+              </Col>
+            
+            </Row>
+            
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
-export default Articles
+export default Article;
