46be14c7cc4df2b2f3219f9ee8682da18058ac24
Passakun <passakun@passakuns-MacBook-Air.local>
2018-07-22 01:17:50 +0700

    Add css style

--- client/components/Instructors.js
+++ client/components/Instructors.js
@@ -2,57 +2,55 @@ import React, { Component } from 'react';
 import { Link } from '../routes';
 import { Row, Col, DemoBox } from 'antd';
 import Imageblog from '../static/images/teacher.jpg?inline';
+import '../styles/Instructor.css';
 
 
 class Instructors extends Component {
   render() {
     return (
-      <div style = {{ marginTop: '30px'}}>
+      <div className='instructor'>
         <h1 style={{ textAlign:'center', height: '10px'}}> ครูผู้สอน </h1>
         <h1 style={{ textAlign:'center', color: '#FFA500'}}> ______ </h1>   
-        <Row gutter={48} style= {{ paddingTop: '20px'}}>
-          <Col span={8} style= {{ textAlign: 'center'}}> 
-          <img
-              src= {Imageblog}
-              style={{
-                width: '80%',
-                height: 250,
-                borderRadius: '50%',
-                marginBottom: '20px',
-              }}
-            /> 
+        <Row gutter={48} className='row-instructor'>
+          <Col span={8} className='column-instructor'> 
+          <Link route='article' params={{id: '1'}}>
+            <a>
+              <img
+                src= {Imageblog}
+                className= 'image-instructor'
+              /> 
+            </a>
+          </Link>
             <div>
               <h3 style={{ marginBottom: '1em'}}> "คณิตศาสตร์มีชีวิต... คณิตศาสตร์อาจารย์โต้ง"</h3>
               <h2 style={{ marginBottom: '0px'}}>อ. โต้ง</h2>
               <h3>คณิตศาสตร์</h3>
             </div>
           </Col>
-          <Col span={8} style= {{ textAlign: 'center'}}> 
-          <img
-              src= {Imageblog}
-              style={{
-                width: '80%',
-                height: 250,
-                borderRadius: '50%',
-                marginBottom: '20px',
-              }}
-            /> 
+          <Col span={8} className='column-instructor'> 
+          <Link route='article' params={{id: '1'}}>
+            <a>
+              <img
+                src= {Imageblog}
+                className= 'image-instructor'
+              /> 
+            </a>
+          </Link>
             <div>
               <h3 style={{ marginBottom: '1em'}}> "คณิตศาสตร์มีชีวิต... คณิตศาสตร์อาจารย์โต้ง"</h3>
               <h2 style={{ marginBottom: '0px'}}>อ. โต้ง</h2>
               <h3>คณิตศาสตร์</h3>
             </div>
           </Col>
-          <Col span={8} style= {{ textAlign: 'center'}}> 
-           <img
-              src= {Imageblog}
-              style={{
-                width: '80%',
-                height: 250,
-                borderRadius: '50%',
-                marginBottom: '20px',
-              }}
-            /> 
+          <Col span={8} className='column-instructor'> 
+          <Link route='article' params={{id: '1'}}>
+            <a>
+              <img
+                src= {Imageblog}
+                className= 'image-instructor'
+              /> 
+            </a>
+          </Link>
             <div>
               <h3 style={{ marginBottom: '1em'}}> "คณิตศาสตร์มีชีวิต... คณิตศาสตร์อาจารย์โต้ง"</h3>
               <h2 style={{ marginBottom: '0px'}}>อ. โต้ง</h2>
